935d135afbe6014a01959323534849a192d09cd4
Alexandr Kilchevskiy <alexkill94@mail.ru>
2023-07-19 10:36:35 +0300

    add user registration

--- radiology_sample/radiology_sample/urls.py
+++ radiology_sample/radiology_sample/urls.py
@@ -20,9 +20,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import routers
-from sample.views import SampleViewSet
-from django.conf import settings
-from django.conf.urls.static import static
+from sample.views import SampleViewSet  # noqa
+from account.views import CreateUserView  # noqa
 
 router = routers.SimpleRouter()
 router.register(r'sample', SampleViewSet)
@@ -44,6 +43,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/', include('rest_framework.urls')),
     path('api/v1/', include(router.urls)),
+    path('api/v1/register', CreateUserView.as_view()),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
