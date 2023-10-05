af56fcf741f47edc78f68c53c86fcd5341ee2095
Alexandr Kilchevskiy <alexkill94@mail.ru>
2023-10-05 21:36:36 +0300

    add unit tests, and search, filters dor get_all_samples updated requirements.txt

--- radiology_sample/sample/tests.py
+++ radiology_sample/sample/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from .models import Sample
 
-# Create your tests here.
+
+class SampleViewSetTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.sample_data = {
+            'title': 'Test Title',
+            'text': 'Test Text',
+            'modality': 'RG',
+            'region_of_interest': 'HEAD',
+            'specialization': 'TRAUMA'
+        }
+        self.sample = Sample.objects.create(**self.sample_data)
+
+    def test_get_all_samples(self):
+        response = self.client.get('/api/v1/sample/get_all_samples/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.context['samples']), 1)
+
+    def test_add_sample(self):
+        new_sample_data = {
+            'title': 'New Test Title',
+            'text': 'New Test Text',
+            'modality': 'CT',
+            'region_of_interest': 'NECK',
+            'specialization': 'PHYSICIAN'
+        }
+        response = self.client.post('/api/v1/sample/add_sample/', new_sample_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Sample.objects.count(), 2)
+
+    def test_get_defined_sample(self):
+        response = self.client.get('/api/v1/sample/1/get_defined_sample/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.context['defined_sample']['title'], self.sample_data['title'])
+
+    def test_filters(self):
+        Sample.objects.create(title='Another Title', text='Another Text', modality='MRI', region_of_interest='THORAX',
+                              specialization='NEUROLOGY')
+
+        response = self.client.get('/api/v1/sample/get_all_samples/', {'title_search': 'Test'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.context['samples']), 1)
+
+        response = self.client.get('/api/v1/sample/get_all_samples/', {'modality_filter': 'RG'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.context['samples']), 1)
+
+        response = self.client.get('/api/v1/sample/get_all_samples/', {'region_of_interest_filter': 'THORAX'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.context['samples']), 1)
+
+        response = self.client.get('/api/v1/sample/get_all_samples/', {'specialization_filter': 'NEUROLOGY'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.context['samples']), 1)
--- radiology_sample/sample/views.py
+++ radiology_sample/sample/views.py
@@ -6,20 +6,53 @@ from .serializers import SampleSerializer
 from .models import Sample
 from django.shortcuts import render
 from .forms import SampleForm
+from django_filters import rest_framework as filters
+from rest_framework import mixins
 
 
-class SampleViewSet(GenericViewSet):
+class SampleFilter(filters.FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains')
+    modality = filters.ChoiceFilter(choices=Sample.MODALITY_CHOICES)
+    region_of_interest = filters.ChoiceFilter(choices=Sample.REGION_OF_INTEREST_CHOICES)
+    specialization = filters.ChoiceFilter(choices=Sample.SPECIALIZATION_CHOICES)
+
+    class Meta:
+        model = Sample
+        fields = ['title', 'modality', 'region_of_interest', 'specialization']
+
+
+class SampleViewSet(mixins.ListModelMixin, GenericViewSet):
     queryset = Sample.objects.all()
     serializer_class = SampleSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = SampleFilter
+    search_fields = ('title',)
 
     @action(methods=['get'], detail=False)
     def get_all_samples(self, request):
-        samples = Sample.objects.all().values()
+        queryset = Sample.objects.all()
+        title_search = request.GET.get('title_search', None)
+        modality_filter = request.GET.get('modality_filter', None)
+        region_of_interest_filter = request.GET.get('region_of_interest_filter', None)
+        specialization_filter = request.GET.get('specialization_filter', None)
+        if title_search:
+            queryset = queryset.filter(title__icontains=title_search)
+        if modality_filter:
+            queryset = queryset.filter(modality=modality_filter)
+        if region_of_interest_filter:
+            queryset = queryset.filter(region_of_interest=region_of_interest_filter)
+        if specialization_filter:
+            queryset = queryset.filter(specialization=specialization_filter)
+        ordering = request.GET.get('ordering', '')
+        if ordering:
+            queryset = queryset.order_by(ordering)
+        samples = queryset.values()
         context = {
             'samples': samples,
             'MODALITY_CHOICES': Sample.MODALITY_CHOICES,
             'REGION_OF_INTEREST_CHOICES': Sample.REGION_OF_INTEREST_CHOICES,
             'SPECIALIZATION_CHOICES': Sample.SPECIALIZATION_CHOICES,
+            'ordering': ordering,
         }
         return render(request, 'sample/get_all_samples.html', context)
 
@@ -39,5 +72,4 @@ class SampleViewSet(GenericViewSet):
                                 template_name='rest_framework/success_template.html')
         else:
             form = SampleForm()
-
         return Response({'form': form}, template_name='rest_framework/sample_form.html')
